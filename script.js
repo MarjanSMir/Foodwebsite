149a0e8dd610f6bea0ace658eaa03869ff484fdf
DESKTOP-V3M3J82\HI USER <MarjanSmirmohammad@outlook.com>
2021-11-17 13:00:19 +0330

    third day first edit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-document.querySelector(".menu").addEventListener('click',()=>{
-    document.querySelector('.target').forEach((item) => {
-        item.classList
-    })
+document.querySelector(".menu").addEventListener('click', () => {
+    let icon = document.getElementsByClassName('icon');
+    let link = document.getElementsByClassName('link');
+    if (document.querySelector('.nav').style.top == "0%" && document.querySelector('.menu').style.top == "22%") {
+        for (var i = 0; i < icon.length; i++) {
+            icon[i].style.opacity = "0";
+        }
+        for (var i = 0; i < link.length; i++) {
+            link[i].style.opacity = "0";
+        }
+        document.querySelector('.nav').style.top = "-119px";
+        document.querySelector('.menu').style.top = "1%";
+    } else {
+        document.querySelector('.nav').style.top = "0%";
+        document.querySelector('.menu').style.top = "22%";
+        for (var i = 0; i < icon.length; i++) {
+            icon[i].style.opacity = "1";
+        }
+        for (var i = 0; i < link.length; i++) {
+            link[i].style.opacity = "1";
+        }
+    }
 })
